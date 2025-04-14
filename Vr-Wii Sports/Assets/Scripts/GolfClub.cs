41d42224f72da1717eb7fe402f7fd12f6159e7dd
Anakin-Lee <anakinlee.2002@gmail.com>
2025-04-14 12:12:50 -0600

    Finished Golf Scene

---
+++ Vr-Wii Sports/Assets/Scripts/GolfClub.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GolfClub : MonoBehaviour
+{
+    public float forceMultiplier = 10f;
+    [SerializeField, Range(0f, 1f)] public float launchAngle = 0f;
+
+    private Vector3 lastPosition;
+    private Vector3 calculatedVelocity;
+
+    private void FixedUpdate()
+    {
+        calculatedVelocity = (transform.position - lastPosition) / Time.fixedDeltaTime;
+        lastPosition = transform.position;
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log("Collision logged");
+
+        if (!collision.gameObject.CompareTag("GolfBall")) return;
+
+        Rigidbody ballRigidbody = collision.gameObject.GetComponent<Rigidbody>();
+        if (ballRigidbody == null) return;
+
+        Vector3 clubVelocity = calculatedVelocity;
+        Vector3 forceDirection = clubVelocity.normalized + Vector3.up * launchAngle;
+        float impactForce = clubVelocity.magnitude * forceMultiplier;
+
+        Debug.Log($"Club velocity: {clubVelocity}, Force Direction: {forceDirection}, Impact Force: {impactForce}");
+
+        ballRigidbody.AddForce(forceDirection * impactForce, ForceMode.Impulse);
+    }
+}
