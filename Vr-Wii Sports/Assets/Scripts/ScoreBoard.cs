e4fc03af8be859a0afff15f3a86e94ef1a7cdbce
Anakin-Lee <anakinlee.2002@gmail.com>
2025-04-16 12:23:24 -0600

    scoreboard script

---
+++ Vr-Wii Sports/Assets/Scripts/ScoreBoard.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class ScoreBoard : MonoBehaviour
+{
+    [SerializeField] private int currentScore = 360;
+    private int strokes = 0;
+    public TMP_Text scoreText;
+    public TMP_Text strokeText;
+
+
+    void Start()
+    {
+        UpdateScoreUI();
+    }
+
+    public void AddScore(int amount)
+    {
+        currentScore -= amount;
+        UpdateScoreUI();
+    }
+    public void strokeCounter()
+    {
+        strokes = strokes + 1;
+        UpdateStrokesUI();
+    }
+
+    private void UpdateScoreUI()
+    {
+        if (scoreText != null) scoreText.text = "Score: " + currentScore;
+    }
+    private void UpdateStrokesUI()
+    {
+        if (strokes != null) strokeText.text = "Strokes: " + strokes;
+    }
+}
+
